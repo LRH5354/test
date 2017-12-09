17ba9e135578b5861bb9eb394d1cbebaa0b6c2f3
LRH5354 <1587910290@qq.com>
2017-12-09 17:22:17 +0800

    包含了一个poi的数据

--- routes/saveResults.js
+++ routes/saveResults.js
@@ -1,3 +1,50 @@
 /**
  * Created by 15879 on 2017/12/5.
  */
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+/*  将数据存到数据库  */
+
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'test'
+});
+//var  sql = 'SELECT * FROM rs';
+var  addSql = "INSERT INTO rs(province,city,title,lng,lat,address,phoneNumber,tags,keyword) VALUES ?";
+connection.connect();
+
+
+router.post('/', function(req, res, next) {
+
+    var addSqlParams=[];
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '');
+    console.log(path);
+    var data =  req.body;
+
+        for (var index in data) {
+            var arr = data[index].split(', ');
+            var temp=[];
+           for(var i=0;i<arr.length;i++){
+             temp[i]=arr[i];
+            }
+            addSqlParams.push(temp);
+        }
+
+            connection.query(addSql,[addSqlParams], function (err, result) {
+                if (err) {
+                    console.log('[INSERT ERROR] - ', err.message);
+                    return;
+                }
+            });
+
+    // });
+    res.writeHead(200,{'content-Type':'text/plain'});
+    res.end("成功");
+ });
+
+
+module.exports = router;
